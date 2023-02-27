f1582b79a89a4019eaa997077f497b349f4af879
Dorian Longépée <dorianlongepee.pro@gmail.com>
2023-02-27 10:27:32 +0100

    add file to vcs

---
+++ src/cards/Card.java
@@ -0,0 +1,17 @@
+package cards;
+
+public class Card implements ICard {
+
+	private boolean isVisible = false;
+    //private boolean isVisible = true;
+	@Override
+	public boolean isVisible() {
+		return this.isVisible;
+	}
+
+	@Override
+	public void flip() {
+		this.isVisible = !this.isVisible();
+	}
+
+}
